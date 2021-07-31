2fe7b9921439680a9fb0339bd014171475d2c98b
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-07-31 17:50:07 +0200

    load to fetch json with given me

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 func load<T: Decodable>(fileName: String) -> T {
-   
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: fileName, withExtension: nil)
+    else { fatalError("Couldn't find \(fileName) in main bundle") }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(fileName) from main bundle: \n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(fileName) as \(T.self): \n\(error)")
+    }
+    
 }
