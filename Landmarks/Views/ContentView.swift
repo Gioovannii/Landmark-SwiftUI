40b3a4b8d3771ca701d8732344237018ca686bf9
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-08-02 10:39:57 +0200

    New file

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Landmark
+//
+//  Created by Giovanni Gaffé on 2021/7/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
