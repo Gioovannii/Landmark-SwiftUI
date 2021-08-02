828f1ae7b5d7cdd5090e21cc0f3231e46ac8430f
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-08-02 10:40:56 +0200

    New file for list

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Giovanni Gaffé on 2021/8/1.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+  
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
