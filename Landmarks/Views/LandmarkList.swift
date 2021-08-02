3559e10a72cc9a413f136d59fdeabd2ca749af75
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-08-02 10:41:22 +0200

    Add navigation view and set title

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-  
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail()) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
     }
 }
 
