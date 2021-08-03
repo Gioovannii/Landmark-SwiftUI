a99c59d255fc675652cb221012738c15573d6746
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-08-03 09:58:02 +0200

    use for each to set device preview

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -23,5 +23,10 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
+        ForEach(["iPhone SE (2nd generation)", "iPhoneXS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
