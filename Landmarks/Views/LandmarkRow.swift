6e75e9af201e0e6c4a497402bc2abafaf9584191
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-08-03 09:58:30 +0200

    resize preview

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -26,6 +26,6 @@ struct LandmarkRow_Previews: PreviewProvider {
             LandmarkRow(landmark: landmarks[0])
             LandmarkRow(landmark: landmarks[1])
         }
-        .previewLayout(.fixed(width: 300, height: 300))
+        .previewLayout(.fixed(width: 300, height: 70))
     }
 }
